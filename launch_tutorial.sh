15033de265318ad6ee1d091387883ea3e6f34829
diogro <diogro@gmail.com>
2016-02-22 19:02:46 -0300

    lauch script

---
+++ launch_tutorial.sh
@@ -0,0 +1,6 @@
+#$!/bin/bash
+
+docker pull diogro/r-tutorial
+export TOKEN=$( head -c 30 /dev/urandom | xxd -p )
+docker run --net=host -d -e CONFIGPROXY_AUTH_TOKEN=$TOKEN --name=proxy jupyter/configurable-http-proxy --default-target http://127.0.0.1:9999
+ docker run -d  --net=host -e CONFIGPROXY_AUTH_TOKEN=$TOKEN -v /var/run/docker.sock:/docker.sock jupyter/tmpnb python orchestrate.py --image='diogro/r-tutorial' --command='start-notebook.sh "--NotebookApp.base_url={base_path} --ip=0.0.0.0 --port={port} --NotebookApp.trust_xheaders=True"'
